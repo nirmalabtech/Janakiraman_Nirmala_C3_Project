a5a3fe20f0ff2639dafc17f45a921ffd00d135a1
Nirmala Janakiraman <nirmalabtech@gmail.com>
2022-06-07 22:49:47 +0530

    Part 2: Solution

---
+++ src/test/java/RestaurantTest.java
@@ -0,0 +1,83 @@
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.time.LocalTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RestaurantTest {
+    RestaurantService service = new RestaurantService();
+    Restaurant restaurant;
+    //REFACTOR ALL THE REPEATED LINES OF CODE
+
+
+    public void setUpMockRestaurant() {
+        LocalTime openingTime = LocalTime.parse("10:00:00");
+        LocalTime closingTime = LocalTime.parse("22:00:00");
+        restaurant = service.addRestaurant("KFC", "Chennai", openingTime, closingTime);
+        restaurant.addToMenu("Zinger Burger",200);
+        restaurant.addToMenu("Family Bucket", 400);
+    }
+
+    //>>>>>>>>>>>>>>>>>>>>>>>>>OPEN/CLOSED<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+    //-------FOR THE 2 TESTS BELOW, YOU MAY USE THE CONCEPT OF MOCKING, IF YOU RUN INTO ANY TROUBLE
+    @Test
+    public void is_restaurant_open_should_return_true_if_time_is_between_opening_and_closing_time(){
+        //WRITE UNIT TEST CASE HERE
+
+        LocalTime inputTime = LocalTime.of(10, 10, 00);
+        setUpMockRestaurant();
+        Restaurant spyRestaurant = Mockito.spy(restaurant) ;
+        Mockito.when(spyRestaurant.getCurrentTime()).thenReturn(inputTime);
+        assertTrue(spyRestaurant.isRestaurantOpen());
+
+
+    }
+
+    @Test
+    public void is_restaurant_open_should_return_false_if_time_is_outside_opening_and_closing_time(){
+        //WRITE UNIT TEST CASE HERE
+        LocalTime inputTime = LocalTime.parse("23:00:00");
+        setUpMockRestaurant();
+        Restaurant spyRestaurant = Mockito.spy(restaurant) ;
+        Mockito.when(spyRestaurant.getCurrentTime()).thenReturn(inputTime);
+        assertFalse(spyRestaurant.isRestaurantOpen());
+
+
+    }
+
+
+    //<<<<<<<<<<<<<<<<<<<<<<<<<OPEN/CLOSED>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+    //>>>>>>>>>>>>>>>>>>>>>>>>>>>MENU<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+    @Test
+    public void adding_item_to_menu_should_increase_menu_size_by_1(){
+        setUpMockRestaurant();
+        int initialMenuSize = restaurant.getMenu().size();
+        restaurant.addToMenu("Sizzling brownie",319);
+        assertEquals(initialMenuSize+1,restaurant.getMenu().size());
+    }
+    @Test
+    public void removing_item_from_menu_should_decrease_menu_size_by_1() throws itemNotFoundException {
+        setUpMockRestaurant();
+        int initialMenuSize = restaurant.getMenu().size();
+        restaurant.removeFromMenu("Zinger Burger");
+        assertEquals(initialMenuSize-1,restaurant.getMenu().size());
+    }
+    @Test
+    public void removing_item_that_does_not_exist_should_throw_exception() {
+        setUpMockRestaurant();
+        assertThrows(itemNotFoundException.class,
+                ()->restaurant.removeFromMenu("French fries"));
+    }
+
+    @Test
+    public void calculate_total_item_cost() {
+        setUpMockRestaurant();
+        int totalPrice = restaurant.calculateTotalOrderValue();
+        assertEquals(600, totalPrice);
+        //System.out.println("Total Order value: Rs." + totalPrice);
+    }
+    //<<<<<<<<<<<<<<<<<<<<<<<MENU>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+}
